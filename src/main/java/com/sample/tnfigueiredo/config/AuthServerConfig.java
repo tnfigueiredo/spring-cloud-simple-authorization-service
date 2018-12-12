25337144047259d3063a2408947c0ddb6d11a496
Thiago Figueiredo <tnfigueiredo@gmail.com>
2018-12-12 00:05:11 -0200

    Creating Authorization Server authentication with usage of a service to validate authorization server user.

--- src/main/java/com/sample/tnfigueiredo/config/AuthServerConfig.java
+++ src/main/java/com/sample/tnfigueiredo/config/AuthServerConfig.java
@@ -3,6 +3,8 @@
  */
 package com.sample.tnfigueiredo.config;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -23,19 +26,11 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter{
 	
-	static final String CLIEN_ID = "myuserapp";
-	static final String CLIENT_SECRET = "myusersecret";
-	static final String GRANT_TYPE_PASSWORD = "password";
-	static final String AUTHORIZATION_CODE = "authorization_code";
-	static final String REFRESH_TOKEN = "refresh_token";
-	static final String IMPLICIT = "implicit";
-	static final String SCOPE_ADMIN = "ADMIN";
-	static final String SCOPE_USER = "USER";
-	static final int ACCESS_TOKEN_VALIDITY_SECONDS = 1*60*60;
-	static final int FREFRESH_TOKEN_VALIDITY_SECONDS = 6*60*60;
-	
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	
+	@Resource(name="clientDetailService")
+	private ClientDetailsService clientDetailsService;
 	
 	/*
 	 * Configuration to encode JWT access token
@@ -63,14 +58,7 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter{
 	 */
 	@Override
 	public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
-		configurer
-			.inMemory()
-			.withClient(CLIEN_ID)
-			.secret("{noop}"+CLIENT_SECRET) //avoinding encription for example case
-			.authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, REFRESH_TOKEN, IMPLICIT )
-			.scopes(SCOPE_ADMIN, SCOPE_USER)
-			.accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS).
-			refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);
+		configurer.withClientDetails(clientDetailsService);
 	}
 	
 	/*
